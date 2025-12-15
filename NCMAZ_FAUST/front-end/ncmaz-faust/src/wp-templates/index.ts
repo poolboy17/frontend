1d386aedb242a576a05279e58520815b82ab63c3
genaromvasquez@gmail.com <genaromvasquez@gmail.com>
2025-12-14 23:21:20 -0600

    fix: restore wp-templates index and config for Faust.js

---
+++ NCMAZ_FAUST/front-end/ncmaz-faust/src/wp-templates/index.ts
@@ -0,0 +1,15 @@
+import archive from './archive'
+import category from './category'
+import main from './main'
+import page from './page'
+import single from './single'
+import tag from './tag'
+
+export default {
+  main,
+  archive,
+  category,
+  page,
+  single,
+  tag,
+}
